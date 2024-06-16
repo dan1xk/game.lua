911ace28ccc95565bc4658f8db998d3352d24b4f
Daniel Roberto <sdanirobert@outlook.com>
2024-06-16 01:06:24 -0300

    fix: adjust vec2

--- utils/vector2.lua
+++ utils/vector2.lua
@@ -2,23 +2,23 @@ local Vector2 = {}
 Vector2.__index = Vector2
 
 -- Constructor
-function Vector2.new(x, y)
+function Vector2:new(x, y)
   local self = setmetatable({}, Vector2)
-  self.x = x
-  self.y = y
+  self.x = x or 0
+  self.y = y or 0
   return self
 end
 
 function Vector2:add(other)
-  return Vector2.new(self.x + other.x, self.y + other.y)
+  return Vector2:new(self.x + other.x, self.y + other.y)
 end
 
 function Vector2:normalize()
   local len = math.sqrt(self.x * self.x + self.y * self.y)
   if len > 0 then
-    return Vector2.new(self.x / len, self.y / len)
+    return Vector2:new(self.x / len, self.y / len)
   else
-    return Vector2.new(0, 0)
+    return Vector2:new(0, 0)
   end
 end
 
