30b2e02bce01c4a8a300d584fabd18d37dd2cb1e
Daniel Roberto <sdanirobert@outlook.com>
2024-07-29 18:42:59 -0300

    windowed screen

--- main.lua
+++ main.lua
@@ -5,51 +5,51 @@ local menu = require("menu")
 local currentScreen = "menu"
 
 function love.load()
-    love.window.setTitle("Lua_game")
-    love.mouse.setVisible(false)
+	love.window.setTitle("Lua_game")
+	love.mouse.setVisible(false)
 
-    love.window.setFullscreen(true, "desktop")
-    love.graphics.setBackgroundColor(0.1, 0.1, 0.1)
+	-- love.window.setFullscreen(true, "desktop")
+	love.graphics.setBackgroundColor(0.1, 0.1, 0.1)
 
-    menu.load(switchContext)
-    game.load(switchContext)
+	menu.load(switchContext)
+	game.load(switchContext)
 
-    updateDimensions()
+	updateDimensions()
 end
 
 function love.update(dt)
-    if currentScreen == "menu" then
-        menu.update(dt)
-    elseif currentScreen == "game" then
-        game.update(dt)
-    end
+	if currentScreen == "menu" then
+		menu.update(dt)
+	elseif currentScreen == "game" then
+		game.update(dt)
+	end
 end
 
 function love.draw()
-    if currentScreen == "menu" then
-        menu.draw()
-    elseif currentScreen == "game" then
-        game.draw()
-    end
+	if currentScreen == "menu" then
+		menu.draw()
+	elseif currentScreen == "game" then
+		game.draw()
+	end
 end
 
 function love.keypressed(key)
-    if currentScreen == "menu" then
-        menu.keypressed(key)
-    elseif currentScreen == "game" then
-        game.keypressed(key)
-    end
+	if currentScreen == "menu" then
+		menu.keypressed(key)
+	elseif currentScreen == "game" then
+		game.keypressed(key)
+	end
 end
 
 function switchContext(context)
-    if context == "menu" then
-        currentScreen = "menu"
-    elseif context == "game" then
-        currentScreen = "game"
-    end
+	if context == "menu" then
+		currentScreen = "menu"
+	elseif context == "game" then
+		currentScreen = "game"
+	end
 end
 
 function updateDimensions()
-    local width, height = love.graphics.getDimensions()
-    game.updateDimensions(width, height)
+	local width, height = love.graphics.getDimensions()
+	game.updateDimensions(width, height)
 end
